2cf62fa98d3a80b70856ed63999c0248663c05f8
dzlk17 <105115971+dzlk17@users.noreply.github.com>
2023-05-22 15:18:45 +0000

    Database connection added

---
+++ src/db.rs
@@ -0,0 +1,11 @@
+use scylla::{Session, SessionBuilder};
+
+use crate::Result;
+
+pub async fn create_session(uri: &str) -> Result<Session> {
+    SessionBuilder::new()
+        .known_node(uri)
+        .build()
+        .await
+        .map_err(From::from)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
-use user_login_api::run;
+mod result;
+mod db;
+mod routes;
+
+use crate::result::Result;
+use routes::create_routes;
 
 #[tokio::main]
-async fn main() {
-    run().await
+async fn main() -> Result<()>{
+    let app = create_routes();
+    let uri = std::env::var("SCYLLA_URI").unwrap_or_else(|_| "127.0.0.1:9042".to_string());
+    //Add ? to check error
+    let session = db::create_session(&uri).await?;
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    .serve(app.into_make_service())
+    .await
+    .unwrap();
+    Ok(())
 }
