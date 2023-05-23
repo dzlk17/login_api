8c3cc037b6826f0fca70cc8741d4700e12568f67
dzlk17 <105115971+dzlk17@users.noreply.github.com>
2023-05-23 00:32:16 +0000

    Created keyspace and table

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,12 +1,12 @@
 mod reg;
 
-use axum::{Router, routing::get, body::Body};
+use axum::{Router, routing::post, body::Body};
 use reg::reg;
 
 /// Function creates all needed routes.
 /// 
 /// Function creates all neeeded endpoints with assigned handlers.
 pub fn create_routes() -> Router<(), Body> {
-    Router::new().route("/reg", get(reg))
+    Router::new().route("/reg", post(reg))
 
 }
