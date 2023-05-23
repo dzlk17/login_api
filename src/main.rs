250ff98787e594114d204a32fe143c2b22f4f130
dzlk17 <105115971+dzlk17@users.noreply.github.com>
2023-05-23 01:49:58 +0000

    Endpoint function returns response

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use routes::create_routes;
 async fn main() -> Result<()>{
     let app = create_routes();
     let session = db::create_session().await?;
-
+    
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
     .serve(app.into_make_service())
     .await
--- src/routes/reg.rs
+++ src/routes/reg.rs
@@ -1,3 +1,12 @@
-pub async fn reg() -> String {
-    "Registration endpoint".to_owned()
+use axum::{Json, http::StatusCode, response::{Response, IntoResponse}};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct User{
+    email: String,
+    password: String,
+}
+pub async fn reg(Json(user): Json<User>) -> Response{
+    let a = Json(user);
+    (StatusCode::CREATED, "Udane".to_owned()).into_response()
 }
